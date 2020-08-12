604b8ebf24313dead0850217eabd4a546fa27b8d
Joonmo koo <spes2357@gmail.com>
2020-08-12 15:10:18 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default () => {
         <meta charSet="utf-8" />
         <title>{title || 'Joonmo Koo Portfolio'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Joonmo Koo Portfolio'} />
       </Helmet>
       <App />
     </>
